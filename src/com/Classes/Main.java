424c5745a4831b5ecd5de7748564dfdac240d2fc
Umer Mehmood <476061@gmail.com>
2020-04-02 19:27:48 +0500

    First Commit
    Main class for testing other classes
    
    last used for MortgageCalculator

---
+++ src/com/Classes/Main.java
@@ -0,0 +1,25 @@
+package com.Classes;
+
+import com.Experimentations.Automobile;
+import com.Refactoring.MortgageCalculator;
+
+import java.text.NumberFormat;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var mortgage = new MortgageCalculator(180_000,4.0,10);
+        System.out.println(mortgage.getMortgage());
+        System.out.println();
+
+        for (double amount: mortgage.getMonthlyBalance() )
+
+            System.out.println(NumberFormat.getCurrencyInstance().format(amount));
+
+    }
+
+
+    public static void showText(TextBox textBox){
+        System.out.println(textBox.text);
+    }
+}
