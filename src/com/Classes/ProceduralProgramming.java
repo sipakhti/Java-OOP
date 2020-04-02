73e1f0c71533e27ba8fbd72c6e79db424e9b2fbf
Umer Mehmood <476061@gmail.com>
2020-04-02 19:32:47 +0500

    First Commit

---
+++ src/com/Classes/ProceduralProgramming.java
@@ -0,0 +1,14 @@
+package com.Classes;
+
+public class ProceduralProgramming {
+
+    public static void main(String[] args) {
+        var employee = new Employee(50_000,20);
+
+
+        employee.calculateWage(15);
+
+    }
+
+
+}
---
+++ src/com/Classes/TextBox.java
@@ -0,0 +1,18 @@
+package com.Classes;
+
+public class TextBox {
+
+    public String text; // Field
+
+    public static void main(String[] args) {
+
+    }
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public void clear() {
+        text = "";
+    }
+}
