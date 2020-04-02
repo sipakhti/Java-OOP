f998cca4880a5bf28f3c8dd9c9b1cc0a8bdc80ca
Umer Mehmood <476061@gmail.com>
2020-04-02 19:30:00 +0500

    First Commit
    Employee class to calculate Wage of the Employee.

---
+++ src/com/Classes/Employee.java
@@ -0,0 +1,45 @@
+package com.Classes;
+
+public class Employee {
+    private int baseSalary, hourlyRate;
+
+    public static int numberOfEmployees;
+
+    public Employee(int baseSalary,int hourlyRate){
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+        numberOfEmployees++;
+    }
+
+    public static void printNumberOfEmployees() {
+        System.out.println(numberOfEmployees);
+    }
+
+    public int calculateWage (int extraHours) {
+        return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public int calculateWage () {
+        return calculateWage(0);
+    }
+
+    private void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less");
+        this.baseSalary = baseSalary;
+    }
+
+    private int getBaseSalary() {
+        return baseSalary;
+    }
+
+    private void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <=0 )
+            throw new IllegalArgumentException("Hourly rate cannot be less than 0;");
+        this.hourlyRate = hourlyRate;
+    }
+
+    private int getHourlyRate() {
+        return hourlyRate;
+    }
+}
